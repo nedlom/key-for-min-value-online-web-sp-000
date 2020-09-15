37b07038bac90a0059d3dabd73b942a1a3a2ed9e
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-15 18:10:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(names_nums)
+  
+  if names_nums == {}
+    nil
+  end
+  
+  if names_nums != {}
+    
+   nums = names_nums.map{|name, num| num}
+    
+    smallest = nums[0]
+    nums.each do |num|
+      smallest = num if num < smallest
+    end
+    
+    person = ""
+    names_nums.each do |name, num| 
+      person = name if names_nums[name] == smallest
+    end
+    
+    person
+  end
 
 end
