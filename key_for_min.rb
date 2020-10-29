d6302be8ba260df7d242316a4ea5fd5fac2e8147
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-29 20:23:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 # =begin
 # def key_for_min_value(names_nums)
   
