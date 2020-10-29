30dad0768ebea114d9261ebe24aa41f17da5250d
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-29 20:21:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+=begin
 def key_for_min_value(names_nums)
   
   if names_nums == {}
@@ -25,9 +25,13 @@ def key_for_min_value(names_nums)
   end
 
 end
+<<<<<<< HEAD
 
 =begin
 
+=======
+  
+>>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
 def key_for_min_value(names_nums)
   
   if names_nums == {}
@@ -51,3 +55,35 @@ def key_for_min_value(names_nums)
   
 end
 =end
+<<<<<<< HEAD
+=======
+
+def invert_hash(hash)
+  new_hash = {}
+  list = hash.collect{|name| name}
+  list.each{|element| new_hash[element[1]] = element[0]}
+  new_hash
+end
+
+def smallest_k(hash)
+  nums = hash.collect{|num, name| num}
+    
+  smallest = nums[0]
+  nums.each do |num|
+    smallest = num if num < smallest
+  end
+end
+
+def key_for_min_value(names_nums)
+  
+  if names_nums == {}
+    nil
+  end
+  
+  if names_nums != {}
+    inverted = invert_hash(names_nums)
+    smallest = smallest_k(inverted)
+    inverted[smallest]
+  end
+end
+>>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
