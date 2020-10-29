213d72f9bbe5c15f3708d4dfa57ff75d32f7eefe
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-29 20:21:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,89 +1,89 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-=begin
-def key_for_min_value(names_nums)
+# =begin
+# def key_for_min_value(names_nums)
   
-  if names_nums == {}
-    nil
-  end
+#   if names_nums == {}
+#     nil
+#   end
   
-  if names_nums != {}
+#   if names_nums != {}
     
-   nums = names_nums.map{|name, num| num}
+#   nums = names_nums.map{|name, num| num}
     
-    smallest = nums[0]
-    nums.each do |num|
-      smallest = num if num < smallest
-    end
+#     smallest = nums[0]
+#     nums.each do |num|
+#       smallest = num if num < smallest
+#     end
     
-    person = ""
-    names_nums.each do |name, num| 
-      person = name if names_nums[name] == smallest
-    end
+#     person = ""
+#     names_nums.each do |name, num| 
+#       person = name if names_nums[name] == smallest
+#     end
     
-    person
-  end
+#     person
+#   end
 
-end
-<<<<<<< HEAD
+# end
+# <<<<<<< HEAD
 
-=begin
+# =begin
 
-=======
+# =======
   
->>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
-def key_for_min_value(names_nums)
+# >>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
+# def key_for_min_value(names_nums)
   
-  if names_nums == {}
-    nil
-  end
+#   if names_nums == {}
+#     nil
+#   end
   
-  if names_nums != {}
+#   if names_nums != {}
     
-    list = names_nums.collect{|name| name}
-    new_hash = {}
-    list.each{|element| new_hash[element[1]] = element[0]}
-    nums = new_hash.collect{|num, name| num}
+#     list = names_nums.collect{|name| name}
+#     new_hash = {}
+#     list.each{|element| new_hash[element[1]] = element[0]}
+#     nums = new_hash.collect{|num, name| num}
     
-    smallest = nums[0]
-    nums.each do |num|
-      smallest = num if num < smallest
-    end
+#     smallest = nums[0]
+#     nums.each do |num|
+#       smallest = num if num < smallest
+#     end
     
-    new_hash[smallest]
-  end
+#     new_hash[smallest]
+#   end
   
-end
-=end
-<<<<<<< HEAD
-=======
+# end
+# =end
+# <<<<<<< HEAD
+# =======
 
-def invert_hash(hash)
-  new_hash = {}
-  list = hash.collect{|name| name}
-  list.each{|element| new_hash[element[1]] = element[0]}
-  new_hash
-end
+# def invert_hash(hash)
+#   new_hash = {}
+#   list = hash.collect{|name| name}
+#   list.each{|element| new_hash[element[1]] = element[0]}
+#   new_hash
+# end
 
-def smallest_k(hash)
-  nums = hash.collect{|num, name| num}
+# def smallest_k(hash)
+#   nums = hash.collect{|num, name| num}
     
-  smallest = nums[0]
-  nums.each do |num|
-    smallest = num if num < smallest
-  end
-end
+#   smallest = nums[0]
+#   nums.each do |num|
+#     smallest = num if num < smallest
+#   end
+# end
 
-def key_for_min_value(names_nums)
+# def key_for_min_value(names_nums)
   
-  if names_nums == {}
-    nil
-  end
+#   if names_nums == {}
+#     nil
+#   end
   
-  if names_nums != {}
-    inverted = invert_hash(names_nums)
-    smallest = smallest_k(inverted)
-    inverted[smallest]
-  end
-end
->>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
+#   if names_nums != {}
+#     inverted = invert_hash(names_nums)
+#     smallest = smallest_k(inverted)
+#     inverted[smallest]
+#   end
+# end
+# >>>>>>> b986c6fbb29a5581b8652e1f0d3cf33e7f1238d5
